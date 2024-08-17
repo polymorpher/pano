24c09261d2a45f6d443fd4054aea0356f7b98e23
polymorpher <aaron@qokka.ai>
2024-08-17 01:08:44 -0700

    useERC20

---
+++ cli/src/token.ts
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import { type Address, getContract, type GetContractReturnType, type PublicClient } from 'viem'
+
+import {
+  IERC20Abi,
+  IERC20MetadataAbi
+} from './constants.js'
+import { usePublicClient } from './client.js'
+
+type IERC20 = GetContractReturnType<typeof IERC20MetadataAbi, PublicClient>
+
+export interface ERC20Metadata {
+  name: string
+  symbol: string
+  decimals: number
+}
+
+export const useERC20 = ({ address }: { address: Address }) => {
+  const { client } = usePublicClient()
+  const [name, setName] = useState<string>('')
+  const [decimals, setDecimals] = useState<number>(0)
+  const [symbol, setSymbol] = useState<string>('')
+  const [contract, setContract] = useState<IERC20>()
+
+  useEffect(() => {
+    async function init () {
+      if (!client) {
+        return
+      }
+      const c = getContract({ abi: IERC20Abi, address, client })
+      const cm = getContract({ abi: IERC20MetadataAbi, address, client })
+      // c.read.
+      setContract(c)
+      const name = await cm.read.name()
+      const symbol = await cm.read.symbol()
+      const decimals = await cm.read.decimals()
+      setName(name)
+      setSymbol(symbol)
+      setDecimals(decimals)
+    }
+    init().catch(console.error)
+  }, [address, client])
+  return { name, decimals, symbol, contract }
+}
