4929c7ab84f0caa728e9df5e836fe52f40169e21
polymorpher <aaron@qokka.ai>
2024-09-03 12:36:57 -0700

    db interactions, and id encoder, decoder

--- cli/src/common.tsx
+++ cli/src/common.tsx
@@ -2,6 +2,7 @@ import React, { type PropsWithChildren } from 'react'
 import { Box, Text } from 'ink'
 import type { Pair } from './config.js'
 import type { Address } from 'viem'
+import type { Tuple } from 'reverse-mirage'
 
 export const SectionTitle = ({ children }: PropsWithChildren) => {
   return <Box borderStyle={'single'} borderColor={'yellow'} paddingX={1}><Text color={'yellow'}>{children}</Text></Box>
@@ -12,4 +13,68 @@ export interface ValidatedPair extends Pair {
   token1Address: Address
   uniswapPoolAddress: Address
   panopticPoolAddress: Address
+}
+
+export interface Leg {
+  asset: 'token0' | 'token1'
+  optionRatio: number
+  position: 'long' | 'short'
+  tokenType: 'token0' | 'token1'
+  riskPartnerIndex: 0 | 1 | 2 | 3
+  tickLower: number
+  tickUpper: number
+}
+
+export type FeeTier = 100 | 500 | 3_000 | 10_000
+
+export type TickSpacing = 1 | 10 | 60 | 200
+
+// adapted from Panoptic SDK
+export const calculateLegId = (leg: Leg, tickSpacing: TickSpacing) => {
+  let id = 0n
+  id |= leg.asset === 'token0' ? 0n : 1n
+  id |= BigInt(leg.optionRatio) << 1n
+  id |= (leg.position === 'short' ? 0n : 1n) << 8n
+  id |= (leg.tokenType === 'token0' ? 0n : 1n) << 9n
+  id |= BigInt(leg.riskPartnerIndex) << 10n
+  id |= BigInt((leg.tickUpper + leg.tickLower) / 2) << 12n
+  id |= BigInt((leg.tickUpper - leg.tickLower) / tickSpacing) << 36n
+  return id
+}
+
+export interface Position {
+  uniswapPoolAddress: Address
+  tickSpacing: TickSpacing
+  legs: Tuple<Leg | undefined, 4>
+}
+
+export function getPoolId (pool: Address): bigint {
+  return (BigInt(pool) >> 96n) & 0xffffffffffffffffn
+}
+
+export function extractPoolId (tokenId: bigint): bigint {
+  return tokenId & 0xffffffffffffffffn
+}
+
+// adapted from Panoptic SDK
+export const calculateTokenId = (
+  uniswapPoolAddress: Address,
+  tickSpacing: TickSpacing,
+  legs: Tuple<Leg | undefined, 4>
+): bigint => {
+  let id = 0n
+  id |= (BigInt(uniswapPoolAddress) >> 96n) & 0xffffffffffffffffn
+  id |= legs[0] ? calculateLegId(legs[0], tickSpacing) << 64n : 0n
+  id |= legs[1]
+    ? calculateLegId(legs[1], tickSpacing) << (64n + 48n)
+    : 0n
+  id |= legs[2]
+    ? calculateLegId(legs[2], tickSpacing) << (64n + 48n + 48n)
+    : 0n
+  id |= legs[3]
+    ? calculateLegId(legs[3], tickSpacing) <<
+      (64n + 48n + 48n + 48n)
+    : 0n
+
+  return id
 }
--- cli/src/db.ts
+++ cli/src/db.ts
@@ -1,7 +1,53 @@
 import PouchDB from 'pouchdb'
 import { defaultDbPath } from './config.js'
 import { cmd } from './cmd.js'
-
+import { type Address } from 'viem'
+import { calculateTokenId, extractPoolId, getPoolId, type Position } from './common.js'
 const dbPath = (cmd.db ?? defaultDbPath) as string
 
 export const db = new PouchDB(dbPath)
+
+// making use of Panoptic SDK to convert to and from tokenId. The implementation can be moved or replicated in this project instead
+// since we are not really using the SDK for its intended purpose (interacting with Panoptic contracts)
+
+const POSITIONS = 'positions'
+
+const END_MARK = '\ufff0'
+
+const makeKey = (...paths: string[]) => {
+  return paths.join('/')
+}
+
+export async function getPosition (address: Address, tokenId: bigint): Promise<Position> {
+  const poolId = extractPoolId(tokenId)
+  const id = makeKey(address, POSITIONS, poolId.toString(16), tokenId.toString(16))
+  const d = await db.get<Position>(id)
+  const { uniswapPoolAddress, tickSpacing, legs } = d
+  return { uniswapPoolAddress, tickSpacing, legs }
+}
+
+export async function readPositions (address: Address, pool?: Address): Promise<Position[]> {
+  const startkey = pool ? makeKey(address, POSITIONS, getPoolId(pool).toString(16)) : makeKey(address, POSITIONS)
+  const docs = await db.allDocs<Position>({
+    startkey,
+    endkey: startkey + END_MARK
+  })
+  return docs.rows.map(d => {
+    if (!d.doc) {
+      return undefined
+    }
+    const { uniswapPoolAddress, tickSpacing, legs } = d.doc
+    return { uniswapPoolAddress, tickSpacing, legs }
+  }).filter(e => e !== undefined) as Position[]
+}
+
+export async function storePosition (address: Address, position: Position): Promise<void> {
+  const tokenId = calculateTokenId(position.uniswapPoolAddress, position.tickSpacing, position.legs)
+  const poolId = getPoolId(position.uniswapPoolAddress)
+  const _id = makeKey(address, POSITIONS, poolId.toString(16), tokenId.toString(16))
+  const doc = {
+    _id,
+    ...position
+  }
+  await db.put(doc)
+}
