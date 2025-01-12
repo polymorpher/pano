ae2ddb9084f961f590286fe30228ed4bad8e417b
jwst9 <johnathonbaw@gmail.com>
2025-01-12 02:02:34 -0700

    update package versions

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from '@/components/provider';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App.tsx'
 
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
+    <ChakraProvider>
       <App />
-    </Provider>
+    </ChakraProvider>
   </StrictMode>,
 )
